492c4b99211d9c5a07e15c95f3d4c6d4839291ad
Saiffer <32097584+Saiffer@users.noreply.github.com>
2020-09-02 23:04:49 -0400

    get ready for netlify

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,15 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class Social extends Component {
-    render() {
-        return (
-            <div class="social">
-            <a href="https://github.com/saiffer" target="_blank"><i class="fab fa-github"></i></a>
-            <a href="https://www.linkedin.com/in/david-aznaurov-19527b139/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
-            </div>
-            )
-        }
-    }
-    
+  render () {
+    return (
+      <div class='social'>
+        <a
+          href='https://github.com/saiffer'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <i class='fab fa-github'>Projects</i>
+        </a>
+        <a
+          href='https://www.linkedin.com/in/david-aznaurov-19527b139/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <i class='fab fa-linkedin-in'>Resume</i>
+        </a>
+      </div>
+    )
+  }
+}
+
 export default Social
-    
--- src/contents/Home.js
+++ src/contents/Home.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import ReactTypingEffect from 'react-typing-effect';
-import profilepic from '../img/me.jpg';
+import React, { Component } from 'react'
+import ReactTypingEffect from 'react-typing-effect'
+import profilepic from '../img/me.jpg'
 import Social from '../components/Social'
 
-
-
 class Home extends Component {
-    render() {
-        return (
-            <div className="condiv home">
-            <img src={profilepic} className="profilepic" alt="myPhoto"></img>
-            <ReactTypingEffect className="typingeffect" text={['I am David Aznaurov','I am a JavaScript Developer']} speed={100} eraseDelay={700}/>
-            <Social />
-            </div>
-            )
-        }
-    }
-    
-    export default Home
-    
+  render () {
+    return (
+      <div className='condiv home'>
+        <ReactTypingEffect
+          className='typingeffect'
+          text={['I am David Aznaurov', 'I am a JavaScript Developer']}
+          speed={100}
+          eraseDelay={700}
+        />
+        <img src={profilepic} className='profilepic' alt='myPhoto'></img>
+        <Social />
+      </div>
+    )
+  }
+}
+
+export default Home
